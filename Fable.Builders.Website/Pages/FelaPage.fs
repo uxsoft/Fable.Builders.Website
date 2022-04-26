1cb98f5de4ff346add3e6023cad4fc49ad3c03d3
Jan Dryk <jan.dryk@merck.com>
2022-04-26 15:55:31 +0200

    add emotion css-in-js library

--- Fable.Builders.Website/Pages/FelaPage.fs
+++ Fable.Builders.Website/Pages/FelaPage.fs
@@ -1,12 +1,16 @@
 module Fable.Builders.Website.Pages.FelaPage
 
+open Fable.Builders.Emotion
 open Fable.Builders.Fela
 open Fable.Builders.React
 open Fable.Builders.AntDesign
+
+open Fable.Core
+open Fable.Core.JsInterop
 open Feliz
 
 [<ReactComponent>]
-let TestComponent () =
+let FelaComponent () =
     let css = Fela.useFela ()
     let cls = css [
         style.backgroundColor color.red
@@ -17,12 +21,29 @@ let TestComponent () =
     
     div {
         className cls
-        str "asd"
+        str "Fela"
+    }
+    
+[<ReactComponent>]
+let EmotionComponent () =
+    div {
+        css [
+            style.backgroundColor color.red
+            style.hover [
+                style.backgroundColor color.blue
+            ]
+        ]
+        str "Emotion"
     }
 
+
+
 let view model =
-    Fela.RendererProvider {
-        renderer (Fela.createRenderer())
-        
-        TestComponent ()
+    Content {
+        Fela.RendererProvider {
+            renderer (Fela.createRenderer())
+            
+            FelaComponent ()
+        }
+        EmotionComponent ()
     }
